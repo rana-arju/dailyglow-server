bc2a4786d56a87e68831201714bcec01feeaadbc
rana-arju <arjurana20@gmail.com>
2026-02-27 16:30:20 +0600

    fix and implement

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,14 +2,9 @@ import express from 'express';
 
 import { AuthRouters } from '../modules/auth/auth.routes';
 import { UsersRoutes } from '../modules/Users/Users.route';
-import { ContactRoutes } from '../modules/Contacts/Contacts.route';
-import { ContactFolderRoutes } from '../modules/ContactFolder/ContactFolder.route';
-import { CampaignRoutes } from '../modules/Campaigns/Campaigns.route';
-import { TrackingRoutes } from '../modules/Tracking/Tracking.route';
 import { DashboardRoutes } from '../modules/Dashboard/Dashboard.route';
 import { AdminRoutes } from '../modules/Admin/Admin.route';
 import { OrderRoutes } from '../modules/Orders/order.routes';
-import emailTemplateRoutes from '../../routes/emailTemplateRoutes';
 
 const router = express.Router();
 
@@ -26,22 +21,7 @@ const moduleRoutes = [
     path: '/admins',
     route: AdminRoutes,
   },
-  {
-    path: '/contacts',
-    route: ContactRoutes,
-  },
-  {
-    path: '/contact-folders',
-    route: ContactFolderRoutes,
-  },
-  {
-    path: '/campaigns',
-    route: CampaignRoutes,
-  },
-  {
-    path: '/track',
-    route: TrackingRoutes,
-  },
+
   {
     path: '/dashboard',
     route: DashboardRoutes,
@@ -49,10 +29,6 @@ const moduleRoutes = [
   {
     path: '/orders',
     route: OrderRoutes,
-  },
-  {
-    path: '/',
-    route: emailTemplateRoutes,
   },
 ];
 
